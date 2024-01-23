253b7ae24aa68bd790ed1856b9ae7bf0430d0e54
Abinayasingadurai <128992194+Abinayasingadurai@users.noreply.github.com>
2024-01-23 23:12:25 +0530

    Add files via upload
    6q_23/1/2024

---
+++ Capitalize.py
@@ -0,0 +1,25 @@
+'''Given a full name, your task is to capitalize the name appropriately.
+
+Input Format
+
+A single line of input containing the full name, .
+
+Constraints
+
+The string consists of alphanumeric characters and spaces.
+Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+
+Output Format
+
+Print the capitalized string, .
+
+Sample Input
+
+chris alan
+Sample Output
+
+Chris Alan'''
+
+import string
+def solve(s):
+    return string.capwords(s,' ')
